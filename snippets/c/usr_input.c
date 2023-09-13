fabc2aed0e6eb7344c8a8248876a38b99df0b5a4
yen <54945977+yenanw@users.noreply.github.com>
2023-09-13 16:32:43 +0200

    Add usr_input.c

---
+++ snippets/c/usr_input.c
@@ -0,0 +1,29 @@
+// https://stackoverflow.com/questions/54055462/what-is-the-vulnerability-in-this-c-code
+int
+main(int argc, char **argv)
+{
+    (void) foo(argv[1]);
+    exit(0);
+}
+
+int
+foo(char *arg)
+{
+    return bar(arg);
+}
+
+int
+bar(char *arg)
+{
+    char lbuf[1024];
+    if (strlen(arg) >= 1024)
+        return -1;
+
+    memset(lbuf, 0, sizeof(lbuf));
+    sprintf(lbuf, "%s", "Welcome: ");
+    read(0, lbuf + strlen(lbuf), sizeof(lbuf) - strlen(lbuf) - 1);
+    printf(lbuf);
+    fflush(stdout);
+
+    return 0;
+}
