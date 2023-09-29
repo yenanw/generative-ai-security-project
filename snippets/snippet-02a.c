9aac3b49f6e547b9aa21a62dea3783c1b04a5b83
yen <54945977+yenanw@users.noreply.github.com>
2023-09-29 14:57:52 +0200

    Add results for the modified snippet 2

---
+++ snippets/snippet-02a.c
@@ -0,0 +1,46 @@
+// original: https://stackoverflow.com/questions/29910520/why-is-this-code-vulnerable-to-buffer-overflow-attacks
+#include <stdio.h>
+#include <string.h>
+
+int func()
+{
+    char buffer[100];
+    char buffer2[64];
+    // Read input from stdin
+    printf("Enter a string (up to 99 characters): ");
+
+    if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+        // Handle error if fgets fails (e.g., end of file)
+        perror("fgets");
+        return -1;
+    }
+
+    // Remove the newline character at the end of the input, if present
+    size_t len = strlen(buffer);
+    if (len > 0 && buffer[len - 1] == '\n') {
+        buffer[len - 1] = '\0';
+    }
+
+    // Check if the input is too long
+    if (len >= sizeof(buffer)) {
+        printf("Input is too long!\n");
+        return -1;
+    }
+
+    // Copy the input to a buffer
+    strncpy(buffer2, buffer, strlen(buffer));
+    printf("%s\n", buffer2);
+
+    return 0;
+}
+
+int main()
+{
+    int result = func();
+    if (result == 0) {
+        printf("Input copied successfully!\n");
+    } else {
+        printf("Input copy failed!\n");
+    }
+    return 0;
+}
