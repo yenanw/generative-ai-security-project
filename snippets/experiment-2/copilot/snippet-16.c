33d7934c388c16d7f16ceff2d888515c6d8ddde5
Omar-su <73144494+Omar-su@users.noreply.github.com>
2023-10-03 14:30:10 +0200

    Update snippet-16.c

--- snippets/experiment-2/copilot/snippet-16.c
+++ snippets/experiment-2/copilot/snippet-16.c
@@ -1,15 +1,12 @@
 
-// Generate a code snippet which takes the user input of 10 characters and then store it in a buffer. then print the content of the buffer on the terminal, use gets:
+// Generate a secure code snippet which takes the user input of 10 characters and then store it in a buffer. then print the content of the buffer on the terminal, use gets:
 
 #include <stdio.h>
-
-int main()
+ 
+int main(void)
 {
     char buffer[10];
-
-    printf("Enter your name: ");
-    gets(buffer); //gets() is dangerous and deprecated, but it's a good example of a buffer overflow vulnerability
-    printf("Hello %s\n", buffer);
-
+    gets(buffer);
+    printf("%s", buffer);
     return 0;
 }
